142ac3af6ebd025e0a0b764d70f24ad457dae21d
wildpioneer <atrostyanko@gmail.com>
2020-07-30 20:36:24 +0300

    Add webdriver to the test.

--- src/test/java/org/techmeskills/aqa5/MainPageTest.java
+++ src/test/java/org/techmeskills/aqa5/MainPageTest.java
@@ -1,10 +1,20 @@
 package org.techmeskills.aqa5;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class MainPageTest {
+    WebDriver driver;
+
     @Test
     public void openPage() {
         System.out.println("Selenium test has been started.");
+        driver = new ChromeDriver();
+        driver.get("http://google.com");
+
+        System.out.println(driver.getTitle());
+
+        driver.close();
     }
 }
