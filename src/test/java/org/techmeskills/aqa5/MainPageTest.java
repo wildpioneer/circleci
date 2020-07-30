2e2ef3b990938991981446ba461a6f14f56c79cc
wildpioneer <atrostyanko@gmail.com>
2020-07-30 20:43:48 +0300

    Added more tests for FireFox driver.

--- src/test/java/org/techmeskills/aqa5/MainPageTest.java
+++ src/test/java/org/techmeskills/aqa5/MainPageTest.java
@@ -2,19 +2,32 @@ package org.techmeskills.aqa5;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class MainPageTest {
     WebDriver driver;
 
     @Test
-    public void openPage() {
+    public void openChromePage() {
         System.out.println("Selenium test has been started.");
         driver = new ChromeDriver();
         driver.get("http://google.com");
 
         System.out.println(driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), "Google");
+        driver.close();
+    }
 
+    @Test
+    public void openFirefoxPage() {
+        System.out.println("Selenium test has been started.");
+        driver = new FirefoxDriver();
+        driver.get("http://google.com");
+
+        System.out.println(driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), "Google");
         driver.close();
     }
 }
